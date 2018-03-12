22bae265b9ba6c5161de728ab7ea029f58db1dd4
Alexander Sawadski <alexander.sawadski@uniberg.com>
2018-03-12 15:32:33 +0100

    Refactor filter function

--- public/lib/filter.js
+++ public/lib/filter.js
@@ -5,17 +5,15 @@
  * The input parameter links contains two fields source and target which reference the 
  * corrisponding nodes in the nodes array.
  * The removal of one node, makes an update of all related links necessary.
- * To avoid this, we convert the input parameters into an map and filter all invalid values.
+ * To avoid this, we add some additionally attributes to the nodes array and then
+ * filter the nodes and links array. 
  * After the filtering the map is converted back into the initial array structure.
  * 
- * #Assumptions
- * The node name must be unique, because it will be used as an identifier. 
- *
  * #Definitions:
  * ##Input Parameter
  *
- * nodes: {array<NODE>}
- *    This is an input parameter and contains an array of NODEs.
+ * nodes: {array<NODE || NEW_NODE>}
+ *    This is an input parameter and contains an array of NODEs or NEW_NODEs.
  *
  * links: {array<LINK>}
  *    This is an input parameter, it contains an array of LINKs.
@@ -29,132 +27,116 @@
  *
  * ##Inner Data Structures
  *
- * linkmap: {map<key, NEW_LINK>}
- *    A linkmap is a map. It is the converted form of the links array.
- *
- * NEW_LINK: { source: <string>, target: <string>, value: <number> }
- *    Same as the LINK object with one difference.
- *    The source and target attributes are the names of the NODE objects.
+ * NEW_NODE: { name: <string>, index: <number>, valid: <boolean> }
+ *    Copy of NODE with two additional attributes index and valid. 
+ *    The index attribute is the old node index, which relates to a LINK.
+ *    The valid attribute is a flag to show which nodes could be deleted.
  */
 module.exports = (function () {
 
   /**
-   * Get node name of the given index.
+   * Is node valid.
+   * Check if node relates to any link.
    *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {number} name - The node index.
-   * @returns {string} - The node name.
+   * @param {number} index - The nodex index.
+   * @param {links} links - The links array.
+   * @returns {boolean} - true if node valid, else false.
    */
-  const _getNodeName = (nodes, index) => nodes[index].name;
+  function _isNodeValid(index, links) {
+    return links.some(link => (link.source === index) || (link.target === index));
+  }
 
   /**
-   * Get node index of the given name.
+   * Get array index of old index.
    *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {string} name - The node name.
-   * @returns {number} - The node index. Returns `-1` if node name not found.
+   * @param {array<NEW_NODE>} nodes - The nodes array with additional attributes.
+   * @param {number} oldIndex - The index of the old nodes array.
+   * @returns {number} - The new nodes array index.
    */
-  const _getNodeIndex = (nodes, name) => nodes.findIndex(node => node.name === name);
+  function _findNodeIndex(nodes, oldIndex) {
+    return nodes.findIndex(({index}) => index === oldIndex);
+  }
 
   /**
-   * Add node if not exist to nodes array.
+   * Filter invalid values from links array.
+   * An invalid value could be a `0` value. 
    *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {string} name - The node name.
-   * @returns {number} - The index of added node 
-   *    or if the name already exists the index of the existing node.
+   * @param {array<LINK>} links - The links array.
+   * @returns {array<Link>} - The filtered links array.
    */
-  const _addNode = (nodes, name) => {
-    let index = _getNodeIndex(nodes, name);
-    if (index < 0) { index = nodes.push({ name }); }
-    return index;
-  };
+  function _filterLinks(links) { return links.filter(({value}) => value); }
 
   /**
-   * Add link to links array.
+   * Filter invalid nodes from nodes array.
+   * Invalid nodes are nodes which are not relates to any link.  
    *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {array<LINK>} links - Array of link objects.
-   * @param {LINK} link - The link object.
-   * @returns {number} - The index of given link.
+   * @param {array<NODE>} nodes - The nodes array.
+   * @param {array<LINK>} links - The links array.
+   * @returns {array<NODE>} - The filtered nodes array.
    */
-  const _addLink = (nodes, links, link) => {
-    return links.push({
-      source: _getNodeIndex(nodes, link.source),
-      target: _getNodeIndex(nodes, link.target),
-      value: link.value
+  function _filterNodes(nodes, links) {
+    return nodes
+    .map(({name}, index) => {
+      const valid = _isNodeValid(index, links);
+      return {index, name, valid};
+    })
+    .filter(node => node.valid);
+  }
+
+  /**
+   * Update source and target node index from links array.
+   *
+   * @param {array<NEW_NODE>} nodes - The nodes array with additional attributes.
+   * @param {array<LINK>} links - The links array.
+   * @return {array<LINK>} - The links array with updated node indices.
+   */
+  function _updateLinks(nodes, links) {
+    return links.map(link => {
+      const source = _findNodeIndex(nodes, link.source);
+      const target = _findNodeIndex(nodes, link.target);
+      return { source, target, value: link.value };
     });
-  };
+  }
 
   /**
-   * Converts a linkmap to an object with two attributes,
-   * Array of nodes and array of links.
+   * Convert nodes array to old structure.
+   * Removed additional attributes from every node.
    *
-   * @param {map} linkmap - The linkmap.
-   * @returns {object} - With all nodes and links.
+   * @param {array<NEW_NODE>} nodes - The nodes array with additional attributes.
+   * @returns {array<NODE>} - The nodes array.
    */
-  const _convertMapToObject = (linkmap) => {
-    const nodes = [];
-    const links = [];
-    linkmap.forEach((link) => {
-      _addNode(nodes, link.source);
-      _addNode(nodes, link.target);
-      _addLink(nodes, links, link);
+  function _convertNodes(nodes) {
+    return nodes.map(({name}) => {
+      return { name };
     });
-    return { nodes, links };
-  };
+  }
 
   /**
-   * Filters invalid values from linkmap.
-   * An invalid value could be a `0` value.
+   * Update links array and convert nodes array to the old structure.
    *
-   * @param {map} linkmap - The linkmap.
-   * @returns {map} - Linkmap without invalid values.
+   * @param {array<NEW_NODE>} nodes - The nodes array with additional attributes.
+   * @param {array<LINK>} links - The links array.
+   * @returns {object} - Valid nodes and links.
    */
-  const _filterLinkMap = (linkmap) => {
-    linkmap.forEach((link, key) => {
-      if (!link.value) {
-        linkmap.delete(key);
-      }
-    });
-    return linkmap;
-  };
-
-  /**
-   * Generate a linkmap.
-   * Generates a key for each link.
-   *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {array<LINK>} links - Array of link objects.
-   * @returns {map} - The linkmap.
-   */
-  const _generateLinkMap = (nodes, links) => {
-    const linkmap = new Map();
-    links.map((link, index) => {
-      linkmap.set(index, {
-        source: _getNodeName(nodes, link.source),
-        target: _getNodeName(nodes, link.target),
-        value: link.value
-      });
-    });
-    return linkmap;
-  };
+  function _updateNodesAndLinks(nodes, links) {
+    return { 
+      links: _updateLinks(nodes, links), 
+      nodes: _convertNodes(nodes)
+    };
+  }
 
   /**
    * Filter invalid nodes and links out.
    *
-   * @param {array<NODE>} nodes - Array of node objects.
-   * @param {array<LINK>} links - Array of link objects.
+   * @param {array<NODE>} nodes - The nodes array.
+   * @param {array<LINK>} links - The links array.
    * @returns {object} - Valid nodes and links.
    */
-  const filterNodesAndLinks = (nodes, links) => {
-    // generate hashmap
-    const linkmap = _generateLinkMap(nodes, links);
-    // delete invalid links and nodes
-    const filteredLinkmap = _filterLinkMap(linkmap);
-    // convert linkmap to links and nodes
-    return _convertMapToObject(filteredLinkmap);
-  };
+  function filterNodesAndLinks(nodes, links) {
+    const filteredLinks = _filterLinks(links);
+    const filteredNodes = _filterNodes(nodes, filteredLinks);
+    return _updateNodesAndLinks(filteredNodes, filteredLinks);
+  }
 
   return { filterNodesAndLinks };
 }());
