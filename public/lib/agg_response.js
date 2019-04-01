14e479f5b75dd7190b50c0b586377a878f460df9
ch-bas <chagra.bassem@yahoo.fr>
2019-04-01 14:34:14 +0200

    *Add of the 'Remove Workaround comments' *Explanation of the workaround

--- public/lib/agg_response.js
+++ public/lib/agg_response.js
@@ -1,3 +1,5 @@
+// TODO: Remove Workaround
+// _array_to_linked_list should be imported from the removed directory 'ui/agg_response/hierarchical/_array_to_linked_list'
 const { arrayToLinkedList } = require('./_array_to_linked_list');
 const { aggregate } = require('./agg_response_helper');
 
@@ -11,6 +13,9 @@ module.exports = function sankeyProvider(Private, Notifier) {
     buckets = arrayToLinkedList(buckets);
     if (buckets && buckets.length > 1) {
       if (resp.rows && resp.rows.length > 0) {
+      	// TODO: Remove Workaround
+      	// In the new kibana version, the rows are of type object , where they should be of type array to match the rest of the algorithm .
+      	// This is a workaround to convert the object ( 'col-0-2' : [array]... ) to (0 : [array])
         var newRows = [];
           resp.rows.map(function(k,v){
             for ( var property in k ) {
@@ -20,6 +25,7 @@ module.exports = function sankeyProvider(Private, Notifier) {
             }
             newRows.push(_.values(k));
             });
+          //end Workaround
             const aggData = newRows;
          return {
           slices: aggregate(aggData)
