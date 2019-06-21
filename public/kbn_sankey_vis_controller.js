c4948dfb71a1e6c935cc1c90d0ccaec088f5d84b
Bassem Chagra <bassem.chagra@uniberg.com>
2019-06-21 16:47:36 +0200

    [sankey] Fix for the graph bug after updating the buckets

--- public/kbn_sankey_vis_controller.js
+++ public/kbn_sankey_vis_controller.js
@@ -28,8 +28,8 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
 
   let _updateDimensions = function _updateDimensions() {
     let delta = 10;
-    let w = $(svgRoot).parent().width() - 10;
-    let h = $(svgRoot).parent().height() - 40;
+    let w = $element.parent().width() - 10;
+    let h = $element.parent().height() - 40;
     if (w) {
       if (w > delta) {
         w -= delta;
@@ -45,106 +45,104 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
   };
 
   let _buildVis = function (data) {
+
     if(!resize){
       data.slices=filterNodesAndLinks(data.slices.nodes, data.slices.links);
     }
     $scope.emptyGraph = (data.slices.nodes.length <= 0) ;
-
-    _updateDimensions();
 
     let energy = data.slices;
     div = d3.select(svgRoot);
     if (!energy.nodes.length) return;
 
     svg = div.append('svg')
-      .attr('width', width)
-      .attr('height', height + margin)
-      .append('g')
-      .attr('transform', 'translate(0, 0)');
+        .attr('width', width)
+        .attr('height', height + margin)
+        .append('g')
+        .attr('transform', 'translate(0, 0)');
 
-    $scope.$on('change:vis', function () {
-      _updateDimensions();
-      _buildVis(globalData);
-    });
+
 
     let sankey = d3.sankey()
-      .nodeWidth(15)
-      .nodePadding(10)
-      .size([width, height]);
+        .nodeWidth(15)
+        .nodePadding(10)
+        .size([width, height]);
 
     let path = sankey.link();
 
     sankey
-      .nodes(energy.nodes)
-      .links(energy.links)
-      .layout(32);
+        .nodes(energy.nodes)
+        .links(energy.links)
+        .layout(32);
 
     let link = svg.append('g').selectAll('.link')
-      .data(energy.links)
-      .enter().append('path')
-      .attr('class', 'link')
-      .attr('d', path)
-      .style('stroke-width', function (d) {
-        return Math.max(1, d.dy);
-      })
-      .sort(function (a, b) {
-        return b.dy - a.dy;
-      });
+        .data(energy.links)
+        .enter().append('path')
+        .attr('class', 'link')
+        .attr('d', path)
+        .style('stroke-width', function (d) {
+          return Math.max(1, d.dy);
+        })
+        .sort(function (a, b) {
+          return b.dy - a.dy;
+        });
 
     link.append('title')
-      .text(function (d) {
-        return d.source.name + ' → ' + d.target.name + '\n' + d.value;
-      });
+        .text(function (d) {
+          return d.source.name + ' → ' + d.target.name + '\n' + d.value;
+        });
 
     let node = svg.append('g').selectAll('.node')
-      .data(energy.nodes)
-      .enter().append('g')
-      .attr('class', 'node')
-      .attr('transform', function (d) {
-        return 'translate(' + d.x + ',' + d.y + ')';
-      })
-      .call(d3.behavior.drag()
-        .origin(function (d) {
-          return d;
+        .data(energy.nodes)
+        .enter().append('g')
+        .attr('class', 'node')
+        .attr('transform', function (d) {
+          return 'translate(' + d.x + ',' + d.y + ')';
         })
-        .on('dragstart', function () {
-          this.parentNode.appendChild(this);
-        })
-        .on('drag', dragmove));
+        .call(d3.behavior.drag()
+            .origin(function (d) {
+              return d;
+            })
+            .on('dragstart', function () {
+              this.parentNode.appendChild(this);
+            })
+            .on('drag', dragmove));
 
     node.append('rect')
-      .attr('height', function (d) {
-        return d.dy;
-      })
-      .attr('width', sankey.nodeWidth())
-      .style('fill', function (d) {
-        d.color = color(d.name);
-        return d.color;
-      })
-      .style('stroke', function (d) {
-        return d3.rgb(d.color).darker(2);
-      })
-      .append('title')
-      .text(function (d) {
-        return d.name + '\n' + d.value;
-      });
+        .attr('height', function (d) {
+          return d.dy;
+        })
+        .attr('width', sankey.nodeWidth())
+        .style('fill', function (d) {
+          d.color = color(d.name);
+          return d.color;
+        })
+        .style('stroke', function (d) {
+          return d3.rgb(d.color).darker(2);
+        })
+        .append('title')
+        .text(function (d) {
+          return d.name + '\n' + d.value;
+        });
 
     node.append('text')
-      .attr('x', -6)
-      .attr('y', function (d) {
-        return d.dy / 2;
-      })
-      .attr('dy', '.35em')
-      .attr('text-anchor', 'end')
-      .attr('transform', null)
-      .text(function (d) {
-        return d.name;
-      })
-      .filter(function (d) {
-        return d.x < width / 2;
-      })
-      .attr('x', 6 + sankey.nodeWidth())
-      .attr('text-anchor', 'start');
+        .attr('x', -6)
+        .attr('y', function (d) {
+          return d.dy / 2;
+        })
+        .attr('dy', '.35em')
+        .attr('text-anchor', 'end')
+        .attr('transform', null)
+        .text(function (d) {
+          return d.name;
+        })
+        .filter(function (d) {
+          return d.x < width / 2;
+        })
+        .attr('x', 6 + sankey.nodeWidth())
+        .attr('text-anchor', 'start');
+
+    resize=false;
 
     function dragmove(d) {
       d3.select(this).attr('transform', 'translate(' + d.x + ',' + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ')');
@@ -155,20 +153,24 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
   var _render = window.render = function (data) {
     d3.select(svgRoot).selectAll('svg').remove();
     _buildVis(data);
+
   };
+  var data;
   $scope.$watch('esResponse', function (resp) {
     if (resp) {
-      var data = sankeyAggResponse($scope.vis, resp);
+      data = sankeyAggResponse($scope.vis, resp);
       globalData = data;
       if (data && data.slices){
+        _updateDimensions();
         _render(data);
       }
-      observeResize($element, function () {
-        if (data) {
-          resize=true;
-          _render(data);
-        }
-      });
+    }
+  });
+  observeResize($element, function () {
+    if (data) {
+      _updateDimensions();
+      resize=true;
+      _render(data);
     }
   });
 });
