6f8be5a155b9920e663548a1b950f15a9e4d513f
Bassem Chagra <bassem.chagra@uniberg.com>
2019-08-28 13:52:57 +0200

    [sankey] Fix the indentations according to the Kibana conventions

--- public/kbn_sankey_vis_controller.js
+++ public/kbn_sankey_vis_controller.js
@@ -45,7 +45,6 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
   };
 
   let _buildVis = function (data) {
-
     if(!resize){
       data.slices=filterNodesAndLinks(data.slices.nodes, data.slices.links);
     }
@@ -56,91 +55,91 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
     if (!energy.nodes.length) return;
 
     svg = div.append('svg')
-        .attr('width', width)
-        .attr('height', height + margin)
-        .append('g')
-        .attr('transform', 'translate(0, 0)');
+      .attr('width', width)
+      .attr('height', height + margin)
+      .append('g')
+      .attr('transform', 'translate(0, 0)');
 
 
 
     let sankey = d3.sankey()
-        .nodeWidth(15)
-        .nodePadding(10)
-        .size([width, height]);
+      .nodeWidth(15)
+      .nodePadding(10)
+      .size([width, height]);
 
     let path = sankey.link();
 
     sankey
-        .nodes(energy.nodes)
-        .links(energy.links)
-        .layout(32);
+      .nodes(energy.nodes)
+      .links(energy.links)
+      .layout(32);
 
     let link = svg.append('g').selectAll('.link')
-        .data(energy.links)
-        .enter().append('path')
-        .attr('class', 'link')
-        .attr('d', path)
-        .style('stroke-width', function (d) {
-          return Math.max(1, d.dy);
-        })
-        .sort(function (a, b) {
-          return b.dy - a.dy;
-        });
+      .data(energy.links)
+      .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', function (d) {
+        return Math.max(1, d.dy);
+      })
+      .sort(function (a, b) {
+        return b.dy - a.dy;
+      });
 
     link.append('title')
-        .text(function (d) {
-          return d.source.name + ' → ' + d.target.name + '\n' + d.value;
-        });
+      .text(function (d) {
+        return d.source.name + ' → ' + d.target.name + '\n' + d.value;
+      });
 
     let node = svg.append('g').selectAll('.node')
-        .data(energy.nodes)
-        .enter().append('g')
-        .attr('class', 'node')
-        .attr('transform', function (d) {
-          return 'translate(' + d.x + ',' + d.y + ')';
+      .data(energy.nodes)
+      .enter().append('g')
+      .attr('class', 'node')
+      .attr('transform', function (d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
+      })
+      .call(d3.behavior.drag()
+        .origin(function (d) {
+          return d;
         })
-        .call(d3.behavior.drag()
-            .origin(function (d) {
-              return d;
-            })
-            .on('dragstart', function () {
-              this.parentNode.appendChild(this);
-            })
-            .on('drag', dragmove));
+        .on('dragstart', function () {
+          this.parentNode.appendChild(this);
+        })
+        .on('drag', dragmove));
 
     node.append('rect')
-        .attr('height', function (d) {
-          return d.dy;
-        })
-        .attr('width', sankey.nodeWidth())
-        .style('fill', function (d) {
-          d.color = color(d.name);
-          return d.color;
-        })
-        .style('stroke', function (d) {
-          return d3.rgb(d.color).darker(2);
-        })
-        .append('title')
-        .text(function (d) {
-          return d.name + '\n' + d.value;
-        });
+      .attr('height', function (d) {
+        return d.dy;
+      })
+      .attr('width', sankey.nodeWidth())
+      .style('fill', function (d) {
+        d.color = color(d.name);
+        return d.color;
+      })
+      .style('stroke', function (d) {
+        return d3.rgb(d.color).darker(2);
+      })
+      .append('title')
+      .text(function (d) {
+        return d.name + '\n' + d.value;
+      });
 
     node.append('text')
-        .attr('x', -6)
-        .attr('y', function (d) {
-          return d.dy / 2;
-        })
-        .attr('dy', '.35em')
-        .attr('text-anchor', 'end')
-        .attr('transform', null)
-        .text(function (d) {
-          return d.name;
-        })
-        .filter(function (d) {
-          return d.x < width / 2;
-        })
-        .attr('x', 6 + sankey.nodeWidth())
-        .attr('text-anchor', 'start');
+      .attr('x', -6)
+      .attr('y', function (d) {
+        return d.dy / 2;
+      })
+      .attr('dy', '.35em')
+      .attr('text-anchor', 'end')
+      .attr('transform', null)
+      .text(function (d) {
+        return d.name;
+      })
+      .filter(function (d) {
+        return d.x < width / 2;
+      })
+      .attr('x', 6 + sankey.nodeWidth())
+      .attr('text-anchor', 'start');
 
     resize=false;
 
@@ -153,7 +152,6 @@ module.controller('KbnSankeyVisController', function ($scope, $element, $rootSco
   var _render = window.render = function (data) {
     d3.select(svgRoot).selectAll('svg').remove();
     _buildVis(data);
-
   };
   var data;
   $scope.$watch('esResponse', function (resp) {
