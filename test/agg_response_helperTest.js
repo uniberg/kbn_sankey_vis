18d08efabd25ee6c68aa3c417b629a0c17e2f221
Bassem Chagra <bassem.chagra@uniberg.com>
2020-12-01 12:27:44 +0100

    [sankey] Fix Broken Sorting for the Nodes

--- test/agg_response_helperTest.js
+++ test/agg_response_helperTest.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 
-const stringify = require('json-stable-stringify');
 const aggResponseHelper = require('../public/lib/agg_response_helper');
 
 describe('aggResponseHelper', function() {
@@ -32,7 +31,7 @@ describe('aggResponseHelper', function() {
         { source: 1, target: 5, value: 90 },
         { source: 0, target: 5, value: 90 },
       ];
-      const result = aggResponseHelper.aggregate(data); 
+      const result = aggResponseHelper.aggregate(data);
       const result_nodes = result.nodes;
       const result_links = result.links;
       assert.notStrictEqual(result_nodes, expected_nodes);
@@ -66,32 +65,9 @@ describe('aggResponseHelper', function() {
         { name: 'node0' },
         { name: 'node0' }
       ];
-      const result = aggResponseHelper.aggregate(data); 
+      const result = aggResponseHelper.aggregate(data);
       const result_nodes = result.nodes;
       assert.notStrictEqual(result_nodes, expected_nodes);
-    });
-  });
-
-  describe('privateFkt_generateRefNodesIdMap', function() {
-    it('should create nodes in the right order', function() {
-      /*
-       * n0 - n3 - n2
-       * n1 - n2 - n1
-       */
-      const data = [
-        [ 'node0', 'node3', 'node2', 90 ],
-        [ 'node1', 'node2', 'node1', 90 ]
-      ];
-      const nodesMap = new Map();
-      nodesMap.set(stringify({"layerIndex":0,"nodeName":"node0"}), 'node0');
-      nodesMap.set(stringify({"layerIndex":1,"nodeName":"node3"}), 'node3');
-      nodesMap.set(stringify({"layerIndex":2,"nodeName":"node2"}), 'node2');
-      nodesMap.set(stringify({"layerIndex":0,"nodeName":"node1"}), 'node1');
-      nodesMap.set(stringify({"layerIndex":1,"nodeName":"node2"}), 'node2');
-      nodesMap.set(stringify({"layerIndex":2,"nodeName":"node1"}), 'node1');
-       
-      const result = aggResponseHelper._generateSortedNodeArray(nodesMap);
-      //console.log('RESULT: ',result);
     });
   });
 
